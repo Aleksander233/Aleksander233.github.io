1f90c1aab302fb19609b72fb761502159da7f510
Aleksander233 <75082120+Aleksander233@users.noreply.github.com>
2021-03-12 19:44:56 +0300

    Create Lesson_18.js

---
+++ Lesson_18.js
@@ -0,0 +1,82 @@
+let age = 14
+undefined
+let days = 365
+undefined
+let hours = 24
+undefined
+let minutes = 60
+undefined
+let ageInHours = hours * days * age
+undefined
+ageInHours
+122640
+let ageInMinutes = ageInHours * minutes
+undefined
+ageInMinutes
+7358400
+let seconds = 60
+undefined
+let ageInSeconds = ageInMinutes * seconds
+undefined
+ageInSeconds
+441504000
+let numberTwo = '2'
+undefined
+let numberThree = 3
+undefined
+numberTwo + numberTwo
+"22"
+numberThree + numberThree
+6
+numberTwo + numberThree
+"23"
+let 'to be or not to be'.length
+VM777:1 Uncaught SyntaxError: Unexpected string
+let notbe = 'to be or not to be'.length
+undefined
+notbe
+18
+let nottobe = 'not to be'.length
+undefined
+nottobe
+9
+notbe + nottobe
+27
+let code1 = 'are'
+undefined
+let code2 = 'tubas'
+undefined
+let code3 = 'unsafe?'
+undefined
+let codeWorld = code1[1] + code2[1] + code3[0]
+undefined
+codeWorld
+"ruu"
+let codeWorld = code1[1] + code2[1] + code3[1]
+undefined
+codeWorld
+"run"
+let fancyString = LoRem iPsuM DoloR amEt 
+VM1402:1 Uncaught SyntaxError: Unexpected identifier
+let fancyString = 'LoRem iPsuM DoloR amEt'
+undefined
+let fancyString = .toUpperCase[0] + .toLowerCase[1,21]
+VM1557:1 Uncaught SyntaxError: Unexpected token '.'
+let fancyString = 'LoRem iPsuM DoloR amEt'
+undefined
+let lowerCase = fancyString.toLocaleLowerCase()
+undefined
+lowerCase
+"lorem ipsum dolor amet"
+let UpperCase = lowerCase.toLocaleUpperCase[0] 
+undefined
+UpperCase
+undefined
+let firstLetter = lowerCase[0].toUpperCase()
+undefined
+firstLetter
+"L"
+let normalString = firstLetter + lowerCase.slice(1)
+undefined
+normalString
+"Lorem ipsum dolor amet"
