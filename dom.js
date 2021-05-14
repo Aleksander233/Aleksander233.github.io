a00c2df226efe06a4e12714d526a6b25a4005b8b
Aleksander233 <75082120+Aleksander233@users.noreply.github.com>
2021-05-14 19:56:12 +0300

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,39 @@
+// console.dir(document)
+// console.log(document.head)
+// console.log(document.body)
+// document.title = 'New Title'
+// console.log(document.title)
+// document.all[8].textContent = "NEW NAVBAR"
+// let navbar = document.getElementById('navbar')
+
+// navbar.innerHTML = 'Changed'
+
+// let Navbar = document.getElementById('main')
+
+// Navbar.innerHTML = 'NEW'
+// let navbar = document.getElementById('navbar')
+// navbar.style.border = '5px solid red'
+// let main = document.getElementById('main')
+//  main.style.backgroundColor = 'pink'
+// let items = document.getElementsByClassName('list-group-item')
+// console.log(items)
+
+// items[1].innerHTML = 'NEW CONTENT'
+// items[1].style.backgroundColor = 'yellow'
+// items[1].style.color = 'red'
+
+// for (let el in items) {
+//     items[el].textContent = el
+//     items[el].style.color = 'red';
+//     items[el].style.backgroundColor = 'yellow';
+// }
+let header = document.querySelector('#navbar')
+header.style.borderBottom = 'solid 5px red'
+let input = document.querySelector('input')
+input.value = 'Hello World'
+let  titles = document.querySelectorAll('.h1')
+console.log(titles)
+for (let el in items) {
+    items[el].style.backgroundColor = 'grey';
+
+}
