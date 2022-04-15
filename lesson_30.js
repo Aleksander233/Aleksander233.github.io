902e93ff9088e34c8337230a1fd2c92b80ae3191
Aleksander233 <75082120+Aleksander233@users.noreply.github.com>
2022-04-15 20:56:10 +0300

    Add files via upload

---
+++ lesson_30.js
@@ -0,0 +1,48 @@
+let road = document.querySelector('#road')
+let status = document.querySelector('#status')
+let Boat = function(name, color, size) {
+    this.element = null;
+    this.name = name;
+    this.color = color;
+    this.width = size;
+    this.height = size / 2;
+    this.posX = 0
+    this.create = function () {
+        this.element = document.createElement('div');
+        this.element.className = 'boat'
+        this.element.style.width = this.width + "px"
+        this.element.style.height = this.height + "px"
+        this.element.style.backgroundColor = this.color
+        this.element.innerHTML = '<h3>' + this.name + '</h3>'
+        road.append(this.element)
+    }
+    this.move = function () {
+        this.posX += Math.random() * 5
+        this.element.style.left = this.posX + "px"
+    }
+    this.checkCollision = function () {
+        if (this.posX + this.width > road.clientWidth) {
+            return true
+        }
+        return false
+    }
+    this.winner = function () {
+        this.element.className = 'boat winner'
+    }
+}
+boat_1 = new Boat('Flyer', 'blue', 100);
+boat_1.create();
+boat_2 = new Boat('Glider', 'red', 100);
+boat_2.create();
+let boat = [boat_1, boat_2];
+gameLoop = setInterval(game, 30);
+function game() {
+    for (let i = 0; i < boat.length; i++) {
+        let b = boat[i]
+        b.move()
+        if (b.checkCollision()) {
+            b.winner()
+            clearInterval(gameLoop)
+        }
+    }
+}
